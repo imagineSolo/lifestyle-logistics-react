216234464b635995ad76f85adbfae445db3d39ff
Tomek <soltom6@gmail.com>
2019-12-15 22:05:47 +0100

    Updates

--- pages/ErrorPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorPage = () => {
-  return <div>Error Page</div>;
-};
-
-export default ErrorPage;
---
+++ precache-manifest.234cc15271a5bf477dd3d0f0fa93ffd9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5baa897ea4c2a78fe65020dc843bcb",
+    "url": "/lifestyle-logistics-react/index.html"
+  },
+  {
+    "revision": "dab4c2417a0d4f107153",
+    "url": "/lifestyle-logistics-react/static/css/2.8c7d2076.chunk.css"
+  },
+  {
+    "revision": "f856926a9bcdbc200399",
+    "url": "/lifestyle-logistics-react/static/css/main.9d01f852.chunk.css"
+  },
+  {
+    "revision": "dab4c2417a0d4f107153",
+    "url": "/lifestyle-logistics-react/static/js/2.1bb7c7cf.chunk.js"
+  },
+  {
+    "revision": "f856926a9bcdbc200399",
+    "url": "/lifestyle-logistics-react/static/js/main.4a30cf10.chunk.js"
+  },
+  {
+    "revision": "4234a63018feb28b535b",
+    "url": "/lifestyle-logistics-react/static/js/runtime-main.16c1422b.js"
+  },
+  {
+    "revision": "d594f0f44c08440c7f4ac3b987a46bc0",
+    "url": "/lifestyle-logistics-react/static/media/adult-automotive-blur-car-13861.d594f0f4.jpg"
+  },
+  {
+    "revision": "2c9806369f5778cffa501d2dcd46cab2",
+    "url": "/lifestyle-logistics-react/static/media/assorted-color-intermodal-containers-2547972.2c980636.jpg"
+  },
+  {
+    "revision": "7994f2e9371caad268ebbe7f688e704a",
+    "url": "/lifestyle-logistics-react/static/media/bg-marmor2.7994f2e9.jpg"
+  },
+  {
+    "revision": "10244b5e23d2df276bf32d90af4009b9",
+    "url": "/lifestyle-logistics-react/static/media/logistics.10244b5e.png"
+  },
+  {
+    "revision": "cbeeac2730f8a1eb66d62a6151d1da50",
+    "url": "/lifestyle-logistics-react/static/media/man-in-white-dress-shirt-sitting-on-black-rolling-chair-840996.cbeeac27.jpg"
+  },
+  {
+    "revision": "f5eea02512b3322120d184b0370f6e6a",
+    "url": "/lifestyle-logistics-react/static/media/person-driving-yellow-forklift-1267337.f5eea025.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lifestyle-logistics-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lifestyle-logistics-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lifestyle-logistics-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lifestyle-logistics-react/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "31ea6d02f5c081b5a45bd4ced2ab85dd",
+    "url": "/lifestyle-logistics-react/static/media/spedition.31ea6d02.png"
+  },
+  {
+    "revision": "4002b7d13d67f6bb766b02d23fff5eef",
+    "url": "/lifestyle-logistics-react/static/media/transport.4002b7d1.png"
+  },
+  {
+    "revision": "b66cc8fd69fc72493ab2497946c99c78",
+    "url": "/lifestyle-logistics-react/static/media/warehouse.b66cc8fd.png"
+  }
+]);
